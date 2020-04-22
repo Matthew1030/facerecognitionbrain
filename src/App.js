c7715693261830d65f7b4aa316d5b5ba95b95957
Matthew <mhisscock@gmail.com>
2020-04-22 06:34:56 -0500

    changed port

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://secure-peak-28658.herokuapp.com/image', {
+          fetch('https://secure-peak-28658.herokuapp.com:3000/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
